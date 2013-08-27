8acbd66b636cb83116dce9ccc9b9301e4c4f8b88
Richard Edwards <richard.edwards2@bbc.co.uk>
2013-08-27 08:31:14 +0100

    Swapped node.js assert lib for Chai

--- test/RouterSpec.js
+++ test/RouterSpec.js
@@ -1,11 +1,11 @@
-var assert       = require('assert'),
+var expect       = require('chai').expect,
     EventEmitter = require('events').EventEmitter,
     Router       = require('../script/Router');
 
 describe('Router', function () {
     var router = new Router({ incoming: EventEmitter });
 
-    it('should recieve a connection', function () {
-        assert.equal(router.incoming, EventEmitter);
-    })
+    it('should recieve an incoming connection', function () {
+        expect(router.incoming).to.equal(EventEmitter);
+    });
 })
