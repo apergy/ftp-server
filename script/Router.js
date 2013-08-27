7d4bbd5a503a968595c252c1a50f110e1aff6039
Richard Edwards <richard.edwards2@bbc.co.uk>
2013-08-27 11:39:46 +0100

    Added tests for data listener

--- script/Router.js
+++ script/Router.js
@@ -1,5 +1,12 @@
+var _ = require('underscore');
+
 var Router = function (options) {
     if (options.incoming) this.incoming = options.incoming;
+    this.incoming.on('data', this.route);
 };
+
+_.extend(Router.prototype, {
+    route: function () {}
+});
 
 module.exports = Router;
--- test/RouterSpec.js
+++ test/RouterSpec.js
@@ -1,13 +1,32 @@
-var assert       = require('chai').assert,
-    EventEmitter = require('events').EventEmitter,
-    Router       = require('../script/Router');
+var assert = require('chai').assert,
+    sinon  = require('sinon'),
+    events = require('events'),
+    Router = require('../script/Router');
 
 suite('Router', function () {
     setup(function () {
-        this.router = new Router({ incoming: EventEmitter });
+        this.connection = new events.EventEmitter;
+        sinon.spy(this.connection, 'on');
+
+        sinon.spy(Router.prototype, 'route');
+        this.router = new Router({ incoming: this.connection });
     });
 
-    test('should recieve an incoming connection', function () {
-        assert.equal(this.router.incoming, EventEmitter);
+    teardown(function () {
+        this.connection.on.restore();
+        this.router.route.restore();
+    });
+
+    test('recieves an incoming connection', function () {
+        assert.equal(this.router.incoming, this.connection);
+    });
+
+    test('listens on incoming connection data', function () {
+        assert.ok(this.connection.on.calledWith('data'));
+    });
+
+    test('attempts to route any incoming data', function () {
+        this.connection.emit('data', 'mock');
+        assert.ok(Router.prototype.route.calledWith('mock'));
     });
 });
